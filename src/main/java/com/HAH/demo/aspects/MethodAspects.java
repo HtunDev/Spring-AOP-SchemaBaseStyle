2d7fe6016bd9b88f6d93e5211a0b238134485747
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-03 15:37:16 +0630

    writing advice

--- src/main/java/com/HAH/demo/aspects/MethodAspects.java
+++ src/main/java/com/HAH/demo/aspects/MethodAspects.java
@@ -1,5 +1,48 @@
 package com.HAH.demo.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class MethodAspects {
+
+	public void beforeInvocation() {
+
+	}
+
+	public void afterInvocation() {
+
+	}
+
+	public void afterReturningInvocation() {
+
+	}
+
+	public void afterThrowingInvocation() {
+
+	}
+
+	public Object aroundInvocation(ProceedingJoinPoint joinPoint) {
+
+		Object result = null;
+
+		try {
+
+			// Before Invocation
+			
+			joinPoint.proceed();
+
+			// After Invocation
+			
+			return result;
+		} catch (Throwable e) {
+			
+			// After throwing invocation
+			
+			throw new RuntimeException(e);
+		} finally {
+			
+			// After returning invocation
+		}
+
+	}
 
 }
